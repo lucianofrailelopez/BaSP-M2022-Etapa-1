ee0e2b2a99a57c5a5c95dff6859203ed0654afcf
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 19:09:38 -0300

    BaSP-2022: ex02-if_else

--- Semana-05/exercises/04_if_else.js
+++ Semana-05/exercises/04_if_else.js
@@ -9,3 +9,33 @@ if(Math.random() >= 0.5) {
 }else{
     alert('Lower than 0,5')
 }
+
+/*b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+i.“Bebe” si la edad es menor a 2 años;
+ii.“Niño” si la edad es entre 2 y 12 años;
+iii.“Adolescente” entre 13 y 19 años;
+iv.“Joven” entre 20 y 30 años;
+v.“Adulto” entre 31 y 60 años;
+vi.“Adulto mayor” entre 61 y 75 años;
+vii.“Anciano” si es mayor a 75 años.
+ */
+
+console.log('Exercises 4.a: if_else');
+
+var age = 23;
+
+if (age < 2) {
+    alert('Bebe');
+}else if(age > 2 && age <= 12) {
+    alert('Niño');
+}else if(age >= 13 && age <= 19) {
+    alert('Adolecente');
+}else if(age >= 20 && age <= 30) {
+    alert('Joven');
+}else if(age >= 31 && age <= 60) {
+    alert('Adulto');
+}else if (age >= 61 && age < 75) {
+    alert('Adulto mayor')
+}else if(age >= 75) {
+    alert('Anciano')
+}
