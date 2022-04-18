17a9a789e24a3d8813e2e008708c77eaf9307943
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 19:31:13 -0300

    BaSP-2022: ex01-for

---
+++ Semana-05/exercises/05_for.js
@@ -0,0 +1,10 @@
+/*a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar
+una alerta utilizando cada una de las palabras. */
+
+console.log('Exercises 5.a: for');
+
+var elementFut = ['pelota', 'arco', 'jugadores','camiseta', 'botines'];
+
+for (let i = 0; i < elementFut.length; i++) {
+    alert(elementFut[i]);
+}
