78c265631acefab783b2046d99c21e9708447ff5
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 14:13:06 -0300

    BaSP-2022: ex02-string

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -1,4 +1,4 @@
-/*Crear una variable de tipo string con al menos 10 caracteres y 
+/*a) Crear una variable de tipo string con al menos 10 caracteres y 
 convertir todo el texto en mayúscula (utilizar toUpperCase).*/
 
 console.log('Exercises 2.a: string');
@@ -6,3 +6,12 @@ console.log('Exercises 2.a: string');
 var stringTen = 'Brooklyn nine';
 
 alert('2.a = ' + stringTen.toUpperCase());
+
+/*b) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 caracteres
+guardando el resultado en una nueva variable (utilizar substring).*/
+
+console.log('Exercises 2.b: string');
+
+var stringFive = stringTen.substring(0,5);
+
+alert('2.b = ' + stringFive);
