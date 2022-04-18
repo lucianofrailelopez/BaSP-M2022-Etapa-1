5d95ac8cf1a0dfce80e22ac8996654773b98847c
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 19:36:10 -0300

    BaSP-2022: ex02-for

--- Semana-05/exercises/05_for.js
+++ Semana-05/exercises/05_for.js
@@ -8,3 +8,12 @@ var elementFut = ['pelota', 'arco', 'jugadores','camiseta', 'botines'];
 for (let i = 0; i < elementFut.length; i++) {
     alert(elementFut[i]);
 }
+
+/*b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra 
+modificada. */
+
+console.log('Exercises 5.b: for');
+
+for (let i = 0; i < elementFut.length; i++) {
+    alert(elementFut[i].substring(0,1).toUpperCase() + elementFut[i].substring(1).toLowerCase());
+}
