4d73dbb34f91b19c9c90bf08763fc4b5679777e1
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 14:39:14 -0300

    BaSP-2022: ex04-string

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -3,7 +3,7 @@ convertir todo el texto en mayúscula (utilizar toUpperCase).*/
 
 console.log('Exercises 2.a: string');
 
-var stringTen = 'Brooklyn nine';
+var stringTen = 'brooklyn nine';
 
 alert('2.a = ' + stringTen.toUpperCase());
 
@@ -24,3 +24,13 @@ console.log('Exercises 2.c: string');
 var stringThree = stringTen.substring(10,13);
 
 alert('2.c = ' + stringThree);
+
+/*d) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra
+en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase,
+toLowerCase y el operador +).*/
+
+console.log('Exercises 2.d: string')
+
+var stringEx_d = stringTen.substring(0,1).toUpperCase() + stringTen.substring(2,13).toLowerCase();
+
+alert('2.d = ' + stringEx_d);
