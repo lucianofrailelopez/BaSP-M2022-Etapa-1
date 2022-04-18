3ad76732c9dc26cabafdd5d18807b7b7162b62f1
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 12:30:04 -0300

    BaSP-2022: ex03-variable and operators

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,7 +1,7 @@
-/* a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor 
+/*a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor 
 de la suma de ambos números en una 3er variable.*/
 
-console.log('Exercises 1: variables and operators');
+console.log('Exercises 1.a: variables and operators');
 
 var firstNum = 10;
 
@@ -9,16 +9,25 @@ var secondNum = 30;
 
 var resultNum = firstNum + secondNum;
 
-alert('1.a =' + resultNum);
+alert('1.a = ' + resultNum);
 
-// b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+//b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
 console.log('Exercises 1.b: variables and operators');
 
-var wordOne = 'Hola';
+var wordOne = 'Luciano';
 
-var wordTwo = 'julian';
+var wordTwo = 'Fraile';
 
 var resultWord = wordOne + wordTwo;
 
-alert('1.b =' + resultWord);
+alert('1.b = ' + resultWord);
+
+/*c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
+guardando el resultado de la suma en una 3er variable (utilizar length).*/
+
+console.log('Exercises 1.c: variables and operators');
+
+var stringlength = wordOne.length + wordTwo.length;
+
+alert('1.c = ' + stringlength);
