baf6ee914d8f86430d06f5b4f9fab57c139b9b4e
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-18 16:36:56 -0300

    BaSP-2022: ex01-arrays

---
+++ Semana-05/exercises/03_arrays.js
@@ -0,0 +1,9 @@
+/*a) Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+"Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log).*/
+
+console.log('Exercises 3.a: arrays');
+
+var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre',
+'Noviembre', 'Diciembre'];
+
+console.log(meses[5], meses[11]);
