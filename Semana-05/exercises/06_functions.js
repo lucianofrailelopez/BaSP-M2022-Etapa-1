eac4a0915dd8a92004976538a591f412e60c8821
lucianofrailelopez <lucianofrailee@gmail.com>
2022-04-19 17:58:24 -0300

    BaSP-2022: ex04-functions

--- Semana-05/exercises/06_functions.js
+++ Semana-05/exercises/06_functions.js
@@ -5,7 +5,7 @@ console.log('Exercises 6.a: functions');
 
 var num1 = 10;
 
-var num2 = 30;
+var num2 = 30.9;
 
 var result;
 
@@ -21,8 +21,8 @@ mostrar una alerta aclarando que uno de los parámetros tiene error y retornar e
 
 console.log('Exercises 6.b: functions');
 
-function validation() {
-    if(isNaN(num1) || isNaN(num2)) {
+function validation(number1, number2) {
+    if(isNaN(number1) || isNaN(number2)) {
         alert('It is not a number');
         return console.log(NaN);
     }else{
@@ -31,7 +31,7 @@ function validation() {
     }
 }
 
-validation();
+validation(num1, num2);
 
 /*c) Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero. */
 
@@ -48,3 +48,28 @@ function validateinteger() {
 }
 
 validateinteger();
+
+/*d) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
+En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado). */
+
+console.log('Exercises 6.d: functions');
+
+function validationNum(number1,number2) {
+    if(Number.isInteger(number1) && Number.isInteger(number2)) {
+        result = num1 + num2;
+        return console.log('6.d = ' + result);
+    }else if(Number.isInteger(number1) == false) {
+        alert('first number is not integer, it will become');
+        num1 = Math.round(number1);
+    }else if(Number.isInteger(number2) == false) {
+        alert('second number is not integer, it will become')
+        num2 = Math.round(number2);
+    }else {
+        alert('Both numbers are integers, they are going to be added');
+    }
+
+    result = num1 + num2;
+    return console.log('6.d = ' + result);
+}
+
+validationNum(num1, num2);
